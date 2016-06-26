ce829290684bb71abe9fc39b289c1668c9a0591e
jaxxzer <jwalser90@gmail.com>
2016-06-26 00:07:27 -0400

    Sub: Add manual mode functions

--- ArduSub/Sub.h
+++ ArduSub/Sub.h
@@ -828,8 +828,8 @@ private:
     void sport_run();
     bool stabilize_init(bool ignore_checks);
     void stabilize_run();
-    bool rov_init(bool ignore_checks);
-    void rov_run();
+    bool manual_init(bool ignore_checks);
+    void manual_run();
     void crash_check();
     void parachute_check();
     void parachute_release();
